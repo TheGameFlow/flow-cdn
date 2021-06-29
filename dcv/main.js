93505b498df581389d24086a112830dff2c97f55
Vishnu Nair P <58590215+VishnuNairP@users.noreply.github.com>
2021-06-29 21:42:22 +0530

    Update main.js

--- dcv/main.js
+++ dcv/main.js
@@ -18,34 +18,49 @@ async function connection_data() {
         return await response.json()
     }
     else {
-        console.log("error")
          return "error"
     }
 }
 
+
+
 async function main() {
 
     data = await connection_data()
-    console.log(data)
-    let connection_token = data.connection_token;
-    let sess_id = data.session_id;
-    let public_ip = data.public_ip;
-    let instance_id = data.instance_id;
+    let connection_token = data.connection_token
+    let sess_id = data.session_id
+    let public_ip = data.public_ip
+    let instance_id = data.instance_id
     console.log("Setting log level to INFO");
     dcv.setLogLevel(dcv.LogLevel.INFO);
 
     serverUrl = "https://" + instance_id + '.gameflow.tech' + ":8443/";
 
     console.log("Starting authentication with", serverUrl);
+    connect(sess_id, connection_token);
 
-// auth = dcv.authenticate(
-//     serverUrl,
-//     {
-//         promptCredentials: onPromptCredentials,
-//         error: onError,
-//         success: onSuccess
-//     }
-// );
-    connect(sess_id, connection_token)
+}
 
+
+function connect(sessionId, authToken) {
+    console.log(sessionId, authToken);
+
+    dcv.connect({
+        url: serverUrl,
+        sessionId: sessionId,
+        authToken: authToken,
+        divId: "dcv-display",
+        callbacks: {
+            firstFrame: () => console.log("First frame received")
+        }
+    }).then(function (conn) {
+        console.log("Connection established!");
+        connection = conn;
+        dcv.enterRelativeMouseMode()
+        if (dcv.relativeMouseModeEnabled) {
+            console.log("relative mouse mode enabled!")
+        }
+    }).catch(function (error) {
+        console.log("Connection failed with error " + error.message);
+    });
 }
